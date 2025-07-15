7ee784c2bdf154dc20577b8a553af96bc59409b7
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-07-15 23:17:48 +0530

    custom fileServer for dir

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,17 +9,19 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+		"./ui/html/partials/nav.html",
 	}
-	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,15 +3,45 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
 
 func main() {
 
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+
 	mux := http.NewServeMux()
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /", home)
 	mux.HandleFunc("POST /snippet/create", createSnippet)
 	mux.HandleFunc("POST /snippet/view", viewSnippet)
 	log.Println("Starting server on: 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
